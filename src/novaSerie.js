79a7b73248cdce6286bea2ae6a2fe459905f3165
Laura Tasso <laurabanhareli@hotmail.com>
2020-05-03 20:57:44 -0300

    edição na página de nova série

--- src/novaSerie.js
+++ src/novaSerie.js
@@ -78,7 +78,7 @@ const NovaSerie = () => {
         </div>
         <br></br>
 
-        <button type='button' className='btn btn-primary' onClick={saveSerie}>Salvar</button>
+        <button type='button' style={{width: '200px'}} className='btn btn-primary' onClick={saveSerie}>Salvar</button>
 
       </form>
     </div>
